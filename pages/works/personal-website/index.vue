a97434b5bc466ddae5fd7e661573d89e60959629
Danny Tan <tandanielm@gmail.com>
2020-04-16 23:27:20 -1000

    Adding gif to dismango page and styling changes

--- pages/works/personal-website/index.vue
+++ pages/works/personal-website/index.vue
@@ -4,11 +4,17 @@
       Personal Website
     </div>
     <div class="sub-title">
-      This website was built as a way to display some of my work as both a developer and musician in hopes of finding people to connect with, like yourself!
+      (Work In Progress)
     </div>
+    <br/>
+    <div class="sub-title">
+      This website is being built as a way to display some of my work as both a developer and musician in hopes of finding people to connect with, like yourself!
+    </div>
+    <br/>
     <div class="description">
-      For this project, I used <a href="https://nuxtjs.org/" target="_blank"><b>Nuxt</b></a>, a progressive framework based on Vue.js. For more information and detailed explanation on how things work, check out the <a href="https://nuxtjs.org/guide" target="_blank"><b>Nuxt.js docs</b></a>.
+      For this project, I am using <a href="https://nuxtjs.org/" target="_blank"><b>Nuxt</b></a>, a progressive framework based on Vue.js. For more information and detailed explanation on how things work, check out the <a href="https://nuxtjs.org/guide" target="_blank"><b>Nuxt.js docs</b></a>.
     </div>
+    <br/>
     <div class="description">
       The site is being hosted on AWS using <a href="https://aws.amazon.com/s3/" target="_blank"><b>S3 (Simple Storage Service)</b></a> and <a href="https://aws.amazon.com/cloudfront/" target="_blank"><b>CloudFront (CDN)</b></a>. I'm also using <a href="https://jenkins.io/" target="_blank"><b>Jenkins</b></a> for automated builds and deployments.
     </div>
@@ -24,33 +30,32 @@ export default {
 <style lang="scss" scoped>
 .works-personal-website {
   font-family: 'Roboto Mono', monospace;
-  padding: 8% 15% 80px 15%;
+  padding: 8% 15% calc(8% + 80px) 15%;
   .title {
     font-size: 52px;
     font-weight: bold;
+    margin-bottom: 20px;
   }
   .sub-title {
     font-size: 22px;
     line-height: 1.3;
-    margin: 20px 0;
   }
   .description {
     font-size: 18px;
     line-height: 1.3;
-    margin: 14px 0;
   }
   a {
     color: inherit;
     text-decoration: none;
   }
   &.md {
-    padding: 8% 10% 80px 10%;
+    padding: 8% 10% calc(8% + 80px) 10%;
   }
   &.sm {
-    padding: 8% 8% 80px 8%;
+    padding: 8% 8% calc(8% + 80px) 8%;
   }
   &.xs {
-    padding: 8% 5% 80px 5%;
+    padding: 8% 5% calc(8% + 80px) 5%;
     .title {
       font-size: 30px;
     }
