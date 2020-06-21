bbaacff4e0e42f0ce23da46d4f61e6a029a6c739
Danny Tan <tandanielm@gmail.com>
2020-06-20 15:42:01 -1000

    Changing resource tag colors

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -42,67 +42,67 @@ export default {
         title: 'Ultimate AWS Certified Developer Associate 2020',
         link: 'https://www.udemy.com/course/aws-certified-developer-associate-dva-c01/',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: 'How I manage my personal finances as a minimalist.',
         link: 'https://www.youtube.com/watch?v=YyaVya4YSwU',
         category: 'Finances',
-        tagColor: '#065ED4'
+        tagColor: '#01A93F'
       }, {
         title: 'What Will You Say When You Don\'t Know?',
         link: 'https://open.spotify.com/episode/6bkqqtvW8rZ8IQcIx8EyPC?si=FsfHsOprQReEKnuwlNeGNA',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: 'How Do I Communicate with Non-Technical Decision Makers?',
         link: 'https://open.spotify.com/episode/0LJijvgFFhWy0KzS9f1RFq?si=POC9sItOSYCJ9g1MUNCkyA',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: 'GapClick',
         link: 'https://gapclick.app/',
         category: 'Drums',
-        tagColor: '#DC7C01'
+        tagColor: '#F97601'
       }, {
         title: 'How Questions Influence Pathways to Solutions',
         link: 'https://open.spotify.com/episode/5AZvgM7LbNRXqALz8SosEN?si=jcdKnEV_QI-e1mc_LULGjA',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: 'Morning Brew',
         link: 'https://www.morningbrew.com/',
         category: 'News',
-        tagColor: '#01A467'
+        tagColor: '#1C7FF2'
       }, {
         title: 'iOS 13 & Swift 5 - The Complete iOS App Development Bootcamp',
         link: 'https://www.udemy.com/course/ios-13-app-development-bootcamp/',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: 'InvestTalk',
         link: 'https://open.spotify.com/show/6RlP2Dft6sX6sPv2YBbZKD?si=a-rsq4eqQoe6qsZEcncBxA',
         category: 'Finances',
-        tagColor: '#065ED4'
+        tagColor: '#01A93F'
       }, {
         title: 'These four “clean code” tips will dramatically improve your engineering team’s productivity',
         link: 'https://engineering.videoblocks.com/these-four-clean-code-tips-will-dramatically-improve-your-engineering-teams-productivity-b5bd121dd150',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: '7 Skills of Highly “Effective” Programmers',
         link: 'https://medium.com/better-programming/7-habits-of-highly-effective-programmers-563ee3b63f33',
         category: 'Career Development',
-        tagColor: '#FF5F6D'
+        tagColor: '#7F4CCE'
       }, {
         title: 'FPU - Financial Peace University',
         link: 'https://www.daveramsey.com/fpu',
         category: 'Finances',
-        tagColor: '#065ED4'
+        tagColor: '#01A93F'
       }, {
         title: 'The Dave Ramsey Show',
         link: 'https://open.spotify.com/show/5exfRPDNCBHmntEkJrlLmX?si=qwAyZmjIRe2dh6LJ8TCrmw',
         category: 'Finances',
-        tagColor: '#065ED4'
+        tagColor: '#01A93F'
       }]
     }
   },
